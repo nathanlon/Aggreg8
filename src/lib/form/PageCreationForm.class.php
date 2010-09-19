d20fd110f71984fccf3ab1a8379659ef2b660cb0
Nathan O'Hanlon <nathan@ProsperTechnologies.com>
2010-09-20 00:46:59 +0100

    latest with link to charity

---
+++ src/lib/form/PageCreationForm.class.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Page form.
+ *
+ * @package    aggreg8
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PageCreationForm extends sfForm
+{
+
+  public function configure()
+  {
+    $this->setWidgets(array(
+      'charity_code'          => new sfWidgetFormInputText(),
+      'just_giving_event_id'  => new sfWidgetFormDoctrineChoice(array('model'=>'JustGivingEvent')),
+      'target_amount'         => new sfWidgetFormInputText(),
+      'short_name'            => new sfWidgetFormInputText(),
+      'title'                 => new sfWidgetFormInputText(array('label'=>'Title')),
+    ));
+
+    $this->setValidators(array(
+      'charity_code'          => new sfValidatorPass(array('required' => false)),
+      'just_giving_event_id'  => new sfValidatorPass(array('required' => false)),
+      'target_amount'         => new sfValidatorNumber(array('required' => false)),
+      'short_name'            => new sfValidatorPass(array('required' => false)),
+      'title'                 => new sfValidatorPass(array('required' => false)),
+    ));
+
+
+    $this->widgetSchema->setNameFormat('page[%s]');
+
+  }
+
+    
+}
